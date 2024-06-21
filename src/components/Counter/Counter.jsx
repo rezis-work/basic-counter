a34b1dc558aa3e2fe08b6cfa194759ade93504a3
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-21 13:20:38 +0400

    remove memo and implement new component for avoid of rerender

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -23,7 +23,7 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo(function Counter({ initialCount }) {
+export default function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -54,6 +54,4 @@ const Counter = memo(function Counter({ initialCount }) {
       </p>
     </section>
   );
-});
-
-export default Counter;
+}
